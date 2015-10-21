92bbbf15c1e91a92a3c4c4796bb7da8712c2be01
djohnson7823 <djohnson7823@001LAB153010815.neisd.ad>
2015-10-21 09:55:33 -0500

    65te

---
+++ src/gitocober/octerber.java
@@ -0,0 +1,20 @@
+package gitocober;
+
+import java.util.Scanner;
+
+public class octerber {
+	public static void main(String[]args){
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println(" why are we here ");
+		String answer = input.toString();
+		{
+			if (answer.equals("life"))
+			System.out.println("you pass");
+			else System.out.println("you fail");
+	
+}
+}
+
+
+	}
